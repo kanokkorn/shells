a26b62802d4dc83a01806d60275a0b37922e1040
kanokkorn <kanokorn@outlook.jp>
2022-09-11 23:10:04 +0700

    essential pkg

---
+++ essence.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/sh
+# essential pkg
+
+# Debian base
+sudo apt install tmux git vim curl
+
+# Arch base
+sudo pacman -S tmux git vim curl
+
+# FreeBSD
+sudo pkg install tmux git vim curl
+
