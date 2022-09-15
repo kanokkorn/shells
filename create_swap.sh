a5a07e86d5435a9ae28fd559e444b73cb059003f
kanokkorn <kanokorn@outlook.jp>
2022-09-15 21:59:03 +0700

    create swapfile

---
+++ create_swap.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/sh
+# create swap
+
+# partition
+dd if=/dev/zero of=/swapfile bs=1M count=512 status=progress
+# give permission
+chmod 0600 /swapfile
+mkswap -U clear /swapfile
+swapon /swapfile
+/etc/fstab
+/swapfile none swap defaults 0 0
