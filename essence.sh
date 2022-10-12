806d5b4fdcbc8758a969e5e6b8cfb2161743a82b
kanokkorn <kanokorn@outlook.jp>
2022-10-12 22:00:07 +0700

    add compilers

--- essence.sh
+++ essence.sh
@@ -2,11 +2,10 @@
 # essential pkg
 
 # Debian base
-sudo apt install tmux git vim curl
+sudo apt install tmux git vim curl clang
 
 # Arch base
-sudo pacman -S tmux git vim curl
+sudo pacman -S tmux git vim curl clang
 
 # FreeBSD
 sudo pkg install tmux git vim curl
-
