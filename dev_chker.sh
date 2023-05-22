f862eafede59c6267fc0aa308e0fc53a7a8483e0
kanokkorn <kanokorn@outlook.jp>
2023-05-22 23:13:18 +0700

    map device ID to `/dev/tty` and print to console

---
+++ dev_chker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# solution from 
+# https://unix.stackexchange.com/questions/144029/command-to-determine-ports-of-a-device-like-dev-ttyusb0
+# TODO: rewrite to be BSD sh compatible
+
+for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
+    (
+        syspath="${sysdevpath%/dev}"
+        devname="$(udevadm info -q name -p $syspath)"
+        [[ "$devname" == "bus/"* ]] && exit
+        eval "$(udevadm info -q property --export -p $syspath)"
+        [[ -z "$ID_SERIAL" ]] && exit
+        echo "/dev/$devname - $ID_SERIAL"
+    )
+done
