30c71de764ad09a0eb027a0cb6ecfe6ca003e10d
kanokkorn <kanokorn@outlook.jp>
2024-01-06 15:14:13 +0700

    add url to solution & new file

---
+++ lxapp-reload.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# make mouse set to match theme on start
+#
+lxappearance &
+sleep 0.12
+killall lxappearance
--- restart_bt.sh
+++ restart_bt.sh
@@ -2,6 +2,7 @@
 
 # restart and reset bluetooth service when `org.bluez.Error.Blocked`
 # or `org.bluez.Error.Busy`
+# from https://stackoverflow.com/questions/68728478/
 
 # toggle bluetooth
 rfkill block bluetooth && rfkill unblock bluetooth
